3c1b59632cc1cc1581d78147ace446a817801cd9
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-20 00:51:40 +0100

    BidList.java: add field and column in model entity

--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,6 +1,12 @@
 package com.nnk.springboot.domain;
 
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -8,5 +14,48 @@ import lombok.Data;
 @Entity
 @Table(name = "bid_list")
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+	// TODO: Map columns in data table BIDLIST with corresponding java fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "bidlist_id")
+	private Integer id;
+	
+	@Column(name = "account")
+	private String account;
+	
+	@Column(name = "type")
+	private String type;
+
+	@Column(name = "bid_quantity ")
+	private Double bidQuantity;
+	
+	@Column(name = "ask_quantity ")
+	private Double askQuantity;
+	
+	@Column(name = "bid")
+	private Double bid;
+	
+	@Column(name = "ask")
+	private Double ask;
+	
+	@Column(name = "benchmark")
+	private String benchmark;
+	
+	@Column(name = "book")
+	private String book;
+	
+	@Column(name = "creation_name")
+	private String creationName;
+	
+	@Column(name = "creation_date")
+	private Timestamp creationDate;
+	
+	@Column(name = " revision_name")
+	private String  revisionName;
+	
+	@Column(name = "revision_date")
+	private Timestamp revisionDate;
+	
+	@Column(name = "deal_name ")
+	private String dealName ;
 }
--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -1,5 +1,6 @@
 package com.nnk.springboot.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ import lombok.Data;
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="id")
     private Integer id;
     
     @NotBlank(message = "Username is mandatory")
