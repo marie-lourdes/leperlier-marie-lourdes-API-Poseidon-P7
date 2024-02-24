6eb16a2c06ec51a2e9656890db7868c681649c1d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-24 20:23:57 +0100

    create  UserMapperImpl

---
+++ src/main/java/com/nnk/springboot/domain/dto/UserMapperImpl.java
@@ -0,0 +1,23 @@
+package com.nnk.springboot.domain.dto;
+
+import org.springframework.stereotype.Component;
+
+import com.nnk.springboot.domain.User;
+
+@Component
+public class UserMapperImpl implements IMapper< UserDTO,  User> {
+	
+	public UserDTO userToUserDTO(User user) {
+		return this.entityToObjectDTO(user);
+	}
+	
+	@Override
+	public UserDTO entityToObjectDTO( User user) {
+		Integer id = user.getId();
+		String username= user.getUsername();
+		String fullName =user.getFullName();
+		String role= user.getRole();
+
+		return new UserDTO(id,username,fullName, role);
+	}
+}
