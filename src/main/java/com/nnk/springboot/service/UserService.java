16147c5dac1f211ba4d7deb5e8da8588262fc32d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-24 20:29:42 +0100

    UserService.java: add all methods CRUD

--- src/main/java/com/nnk/springboot/service/UserService.java
+++ src/main/java/com/nnk/springboot/service/UserService.java
@@ -1,11 +1,16 @@
 package com.nnk.springboot.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nnk.springboot.domain.User;
+import com.nnk.springboot.domain.dto.UserDTO;
+import com.nnk.springboot.domain.dto.UserMapperImpl;
 import com.nnk.springboot.repositories.IUserRepository;
 
 @Service
@@ -14,42 +19,50 @@ public class UserService {
 
 	@Autowired
 	private IUserRepository userRepository;
-	
+
+	@Autowired
+	private UserMapperImpl mapper;
+
 	@Autowired
 	private ValidatorPasswordImpl validatorPassword;
-	
-	public User addUser(User userCreated)
-			throws NullPointerException {
+
+	public User addUser(User userCreated) throws NullPointerException {
 		User userRegistered = new User();
-		
+
 		try {
 			if (userCreated == null) {
-				throw new IllegalArgumentException("Empty data of User " + userCreated + " provided and created");	
-			}
-			else {
-				boolean isPasswordUserValid= validatorPassword.validPassword(userCreated.getPassword());
-					if(isPasswordUserValid) {
-						throw new IllegalArgumentException("Password of User " + userCreated + " provided is incorrect");
-					}
+				throw new IllegalArgumentException("Empty data of User " + userCreated + " provided and created");
+			} else {
+				boolean isPasswordUserValid = validatorPassword.validPassword(userCreated.getPassword());
+				if (isPasswordUserValid) {
+					throw new IllegalArgumentException("Password of User " + userCreated + " provided is incorrect");
+				}
 				userRegistered.setUsername(userCreated.getUsername());
 				userRegistered.setPassword(userCreated.getPassword());
 				userRegistered.setFullName(userCreated.getFullName());
 				userRegistered.setRole(userCreated.getRole());
-				
+
 			}
 		} catch (IllegalArgumentException e) {
 			log.error(e.getMessage());
 		}
-		
+
 		userRegistered = userRepository.save(userRegistered);
 		return userRegistered;
 	}
 
 	public UserDTO getUserById(Integer id) throws NullPointerException {
 		User userFoundById = userRepository.findById(id)
-				.orElseThrow(() -> new NullPointerException("Bid " + id + " not found"));
-		UserDTO userDTO = mapper.bidListToUserDTO(userFoundById);
+				.orElseThrow(() -> new NullPointerException("User" + id + " not found"));
+		UserDTO userDTO = mapper.userToUserDTO(userFoundById);
 		return userDTO;
+	}
+
+	public User getUserEntityById(Integer id) throws NullPointerException {
+		User userFoundById = userRepository.findById(id)
+				.orElseThrow(() -> new NullPointerException("User" + id + " not found"));
+
+		return userFoundById;
 	}
 
 	public List<UserDTO> getAllUsers() throws NullPointerException {
@@ -59,27 +72,27 @@ public class UserService {
 			return new ArrayList<>();
 		}
 		allUsers.forEach(bid -> {
-			allUserDto.add(mapper.bidListToUserDTO(bid));
+			allUserDto.add(mapper.userToUserDTO(bid));
 		});
 		return allUserDto;
 	}
 
-	public User updateUserById(Integer id, User bidListUpdated)
-			throws NullPointerException, IllegalArgumentException {
-		User bidListToUpdate = new User();
-		bidListToUpdate = userRepository.findById(id)
-				.orElseThrow(() -> new NullPointerException("Bid " + id + " not found for updating"));
+	public User updateUserById(Integer id, User userUpdated) throws NullPointerException, IllegalArgumentException {
+		User userToUpdate = new User();
+		userToUpdate = userRepository.findById(id)
+				.orElseThrow(() -> new NullPointerException("User" + id + " not found for updating"));
 
-		bidListToUpdate.setAccount(bidListUpdated.getAccount());
-		bidListToUpdate.setType(bidListUpdated.getType());
-		bidListToUpdate.setBidQuantity(bidListUpdated.getBidQuantity());
-		bidListToUpdate = userRepository.save(bidListToUpdate);
-		return bidListToUpdate;
+		userToUpdate.setUsername(userUpdated.getUsername());
+		userToUpdate.setPassword(userUpdated.getPassword());
+		userToUpdate.setFullName(userUpdated.getFullName());
+		userToUpdate.setRole(userUpdated.getRole());
+		userToUpdate = userRepository.save(userToUpdate);
+		return userToUpdate;
 	}
 
 	public void deleteUserById(Integer id) throws NullPointerException {
 		userRepository.findById(id)
-				.orElseThrow(() -> new NullPointerException("Bid " + id + " not found for deleting"));
+				.orElseThrow(() -> new NullPointerException("User" + id + " not found for deleting"));
 		userRepository.deleteById(id);
 	}
 }
