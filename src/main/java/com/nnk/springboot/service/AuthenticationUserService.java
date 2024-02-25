15af68c34aab705a3597ba074de6ce0bed7a1c2e
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-25 23:44:49 +0100

    create class AutheneticationUserService with inter UserDetailService

---
+++ src/main/java/com/nnk/springboot/service/AuthenticationUserService.java
@@ -0,0 +1,9 @@
+package com.nnk.springboot.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationUserService implements UserDetailsService{
+
+}
