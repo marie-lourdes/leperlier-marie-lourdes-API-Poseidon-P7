98d689dcac72f9ad470c6077e82e57902e68bd4d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-28 15:15:26 +0100

    HomeController.java: refactor method with constants link of package utils

--- src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -4,20 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.nnk.springboot.utils.Constants;
+
 @Controller
-public class HomeController
-{
+public class HomeController {
 	@GetMapping("/")
-	public String home(Model model)
-	{
+	public String home(Model model) {
 		return "home";
 	}
 
 	@GetMapping("/admin/home")
-	public String adminHome(Model model)
-	{
-		return "redirect:/bidList/list";
+	public String adminHome(Model model) {
+		return  Constants.BIDLIST_PAGE;
 	}
-
 
 }
